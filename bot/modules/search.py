bef7514e4a8869a32cb2f09715364e99727c26ff
Prashant Meena <71384284+Prashant-blip-cell@users.noreply.github.com>
2021-03-28 10:38:45 +0530

    Update search.py

--- bot/modules/search.py
+++ bot/modules/search.py
@@ -145,7 +145,7 @@ def searchhelp(update, context):
 • /sts <i>[search query]</i>
 • /sukebei <i>[search query]</i>
 '''
-    update.effective_message.reply_photo("https://telegra.ph/file/db03910496f06094f1f7a.jpg", help_string, parse_mode=ParseMode.HTML)
+    update.effective_message.reply_photo("https://telegra.ph/file/7a38dc0650b11ceced3b1.png", help_string, parse_mode=ParseMode.HTML)
     
     
 SEARCHHELP_HANDLER = CommandHandler("tshelp", searchhelp)
